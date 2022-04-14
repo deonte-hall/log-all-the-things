f0879dd4e95511ae7b344ec40a807459309179fc
Deonte H (Sasko) <deonte@Saskos-MacBook-Pro.local>
2022-04-14 14:04:51 -0700

    Minor startup changes to app.js

--- server/app.js
+++ server/app.js
@@ -1,19 +1,35 @@
 const express = require('express');
+const morgan = require('morgan');
 const fs = require('fs');
+//const csv = require('./log.csv')
+const bodyParser = require('body-parser');
+const PORT = 3000;
+
+
+
 const app = express();
 
-app.use((req, res, next) => {
-// write your logging code here
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use(morgan('combined'));
 
+
+app.use((req, res, next) => {
+// logging code here
+   // console.log("this method", req.method);
+    next();
 });
 
 app.get('/', (req, res) => {
-// write your code to respond "ok" here
+    //res.sendFile('.server/welcome.html');
+    //console.log("this is log" , fs.readFile('./log.csv', sendFile(file){
+    //    console.log("file is logged");
+    //}))
 
 });
 
 app.get('/logs', (req, res) => {
-// write your code to return a json object containing the log data here
+//return a json object containing the log data here
 
 });
 
--- server/index.js
+++ server/index.js
@@ -1,5 +1,5 @@
 const server = require('./app');
 
 server.listen(3000, () => {
-  console.log('server is listening on 3000');
+  console.log('server is up, and listening on port 3000');
 })
